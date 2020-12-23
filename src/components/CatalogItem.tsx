8be4e3682f6e79f1d5f2dc498667b3c0c88bd67e
Fernando Corrêa da Silva <fernando_silvasc@hotmail.com>
2020-12-23 08:50:46 -0800

    finished application and refactoring

--- src/components/CatalogItem.tsx
+++ src/components/CatalogItem.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable no-shadow */
 import React, { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { IProduct } from '../store/modules/cart/types';
-import { addProductToCart } from '../store/modules/cart/actions';
+import { addProductToCartRequest } from '../store/modules/cart/actions';
+import { IState } from '../store';
 
 interface CatalogItemProps {
   product: IProduct;
@@ -13,8 +14,12 @@ const CatalogItem: React.FC<CatalogItemProps> = ({
 }: CatalogItemProps) => {
   const dispatch = useDispatch();
 
+  const hasFailedStockCheck = useSelector<IState, boolean>(state => {
+    return state.cart.failedStockCheck.includes(product.id);
+  });
+
   const handleAddProductToCart = useCallback(() => {
-    dispatch(addProductToCart(product));
+    dispatch(addProductToCartRequest(product));
   }, [dispatch, product]);
 
   return (
@@ -25,6 +30,8 @@ const CatalogItem: React.FC<CatalogItemProps> = ({
       <button type="button" onClick={handleAddProductToCart}>
         Add to cart
       </button>
+
+      {hasFailedStockCheck && <span style={{ color: 'red' }}>Sold out</span>}
     </article>
   );
 };
